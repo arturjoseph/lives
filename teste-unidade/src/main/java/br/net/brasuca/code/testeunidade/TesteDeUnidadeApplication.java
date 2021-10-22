712fdad61655d7509b2dc6753794e82865c8b1ca
Artur Neto <artur.neto@brasuca.net.br>
2021-10-22 15:49:46 -0300

    primeiro commit

---
+++ teste-unidade/src/main/java/br/net/brasuca/code/testeunidade/TesteDeUnidadeApplication.java
@@ -0,0 +1,13 @@
+package br.net.brasuca.code.testeunidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDeUnidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteDeUnidadeApplication.class, args);
+	}
+
+}
---
+++ teste-unidade/src/test/java/br/net/brasuca/code/testeunidade/TesteDeUnidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.brasuca.code.testeunidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDeUnidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
