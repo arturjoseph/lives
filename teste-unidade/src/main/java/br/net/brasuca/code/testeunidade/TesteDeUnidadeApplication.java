48a8eb8d5556fa196049aad24349046367627338
Artur Neto <artur.neto@brasuca.net.br>
2021-10-29 20:28:03 -0300

    Correção erro ApplicationStart

--- teste-unidade/src/main/java/br/net/brasuca/code/testeunidade/TesteDeUnidadeApplication.java
+++ teste-unidade/src/main/java/br/net/brasuca/code/testeunidade/TesteDeUnidadeApplication.java
@@ -1,45 +1,13 @@
 package br.net.brasuca.code.testeunidade;
 
-import br.net.brasuca.code.testeunidade.modelo.Endereco;
-import br.net.brasuca.code.testeunidade.modelo.Pessoa;
-import br.net.brasuca.code.testeunidade.modelo.Telefone;
-import br.net.brasuca.code.testeunidade.repositorio.EnderecoRepository;
-import br.net.brasuca.code.testeunidade.repositorio.PessoaRepository;
-import br.net.brasuca.code.testeunidade.repositorio.TelefoneRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
-public class TesteDeUnidadeApplication implements CommandLineRunner {
+public class TesteDeUnidadeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TesteDeUnidadeApplication.class, args);
 	}
 
-	@Autowired
-	private PessoaRepository pessoaRepo;
-	@Autowired
-	private EnderecoRepository enderecoRepo;
-	@Autowired
-	private TelefoneRepository telefoneRepo;
-
-	@Override
-	public void run(String... args) throws Exception {
-		Pessoa artur = new Pessoa("Artur","12345678910");
-		Pessoa ze = new Pessoa("Ze","78945612310");
-		this.pessoaRepo.saveAll(Arrays.asList(artur,ze));
-
-		Endereco end1 = new Endereco("Rua 1",10,"casa 1","bairro 1","cidade 1", "SP",artur);
-		Endereco end2 = new Endereco("Rua 2",20,"casa 2","bairro 2","cidade 1", "SP",ze);
-		this.enderecoRepo.saveAll(Arrays.asList(end1,end2));
-
-		Telefone tel1 = new Telefone("011","912345678",artur);
-		Telefone tel2 = new Telefone("011","987654321",ze);
-		this.telefoneRepo.saveAll(Arrays.asList(tel1,tel2));
-
-	}
 }
--- teste-unidade/src/test/java/br/net/brasuca/code/testeunidade/repositorio/PessoaRepositoryTest.java
+++ teste-unidade/src/test/java/br/net/brasuca/code/testeunidade/repositorio/PessoaRepositoryTest.java
@@ -1,5 +1,49 @@
 package br.net.brasuca.code.testeunidade.repositorio;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import br.net.brasuca.code.testeunidade.modelo.Pessoa;
+import br.net.brasuca.code.testeunidade.util.PessoaCreator;
+
+@DataJpaTest
+@DisplayName("Testes para o repositório Pessoa")
 public class PessoaRepositoryTest {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	@Test
+	@DisplayName("Deve salvar Pessoa com sucesso")
+	void deveSalvarPessoaComSucesso() {
+		// instacias
+		Pessoa pessoa = PessoaCreator.criarPessoaParaSerPersistido();
+		//execução
+		Pessoa pessoaPersistida = this.pessoaRepository.save(pessoa);
+		//verifica
+		assertThat(pessoaPersistida).isNotNull();
+		assertThat(pessoaPersistida.getCodigo()).isNotNull();
+		assertThat(pessoaPersistida.getNome()).isEqualTo(pessoa.getNome());
+	}
+	
+	 @Test
+	 @DisplayName("Deve remover Pessoa com sucesso")
+	 void deveRemoverPessoaComSucesso() {
+		 Pessoa pessoa = PessoaCreator.criarPessoaParaSerPersistido();
+		 Pessoa pessoaPersistida = this.pessoaRepository.save(pessoa);
+
+	        this.pessoaRepository.delete(pessoaPersistida);
+	     
+	        Optional<Pessoa> pessoaOptional = this.pessoaRepository
+	        		.findById(pessoaPersistida.getCodigo());
+
+	        assertThat(pessoaOptional).isEmpty();
+	    }
 
 }
